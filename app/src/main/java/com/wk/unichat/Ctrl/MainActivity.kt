66870754b5c26c29816cde72732d171e0a5fb2b8
Wokub <wojtekkubiak@outlook.com>
2018-12-04 18:45:27 +0100

    Turning off back button at MainActivity

--- app/src/main/java/com/wk/unichat/Ctrl/MainActivity.kt
+++ app/src/main/java/com/wk/unichat/Ctrl/MainActivity.kt
@@ -11,6 +11,7 @@ import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
+import android.view.KeyEvent
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.ArrayAdapter
@@ -79,6 +80,17 @@ class MainActivity : AppCompatActivity(){
             drawer_layout.closeDrawer(GravityCompat.START)
             downloadChannelData()
         }
+    }
+
+    // Wyłączenie przycisku wstecz dla MainActivity
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        var backButton = true
+
+        if(!backButton) {
+            return super.onKeyDown(keyCode, event)
+        }
+
+        return false
     }
 
     // Rozłączenie z socketem
